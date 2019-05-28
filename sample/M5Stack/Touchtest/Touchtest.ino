a99e5d72fd7d924e685f7dc627d343d0adb1c5d7
Tetsuya Dota <dota@surface-arch.com>
2019-05-28 20:46:15 +0900

    Update graph

--- sample/M5Stack/Touchtest/Touchtest.ino
+++ sample/M5Stack/Touchtest/Touchtest.ino
@@ -2,8 +2,9 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "MTCH6102.h"
-
 #define ADDR 0x25
+#define ScreenWidth 320
+#define ScreenHeight 240
 
 MTCH6102 mtch = MTCH6102();
 int len = 8;
@@ -42,54 +43,77 @@ void setup() {
   
   mtch.writeRegister(MTCH6102_CMD, 0x20);
   delay(500);
+
+  
 }
 
 void loop() {
-//M5.lcd.clear();
-M5.Lcd.setCursor(0, 70);
+  //M5.lcd.clear();
+  M5.Lcd.setCursor(0, 70);
   
   byte data; 
   float sensVals[10]={0,0,0,0,0,0,0,0,0,0};
+
+  //背景ライン
+  for(int i=0; i<len; i++){
+    M5.Lcd.drawLine((i+1)*35, ScreenHeight-40, (i+1)*35, 0, 0x0000cc);
+  }
+  for(int i=1; i<11; i++){
+    M5.Lcd.drawLine(0, i*20, ScreenWidth, i*20, 0x0000cc);
+  }
 
   Serial.print("SENSORVALUE_RX <i>: ");
   //for (byte i = MTCH6102_SENSORVALUE_RX0; i < MTCH6102_SENSORVALUE_RX0+10; i++) {
   for (int i = 0; i < len; i++) {
      data = mtch.readRegister(MTCH6102_SENSORVALUE_RX0+i);
      
-    //Serial.print(data);
-    //Serial.print(", ");
+    Serial.print(data);
+    Serial.print(", ");
 
     sensVals[i] = data;
+
+    M5.Lcd.fillRect(30+(i*35), ScreenHeight-20, 30, 10, TFT_BLACK);
+    M5.Lcd.setCursor(30+(i*35), ScreenHeight-20);
     M5.Lcd.print(data);
-    M5.Lcd.print(", ");
-  }
-  
-  for (int i = 0; i<len; i++){
-    float prev;
-    if(i==0){
-       prev = 0;
-    }else{
-       prev=sensVals[i-1];
-    }
-    M5.Lcd.drawLine(i*30, 240-(prev/255)*240, (i+1)*30, 240-(sensVals[i]/255)*240, TFT_WHITE);
-  }
-  delay(100);
-  
-  for (int i = 0; i<len; i++){
-    float prev;
-    if(i==0){
-       prev = 0;
-    }else{
-       prev=sensVals[i-1];
-    }
-    M5.Lcd.drawLine(i*30, 240-(prev/255)*240, (i+1)*30, 240-(sensVals[i]/255)*240, TFT_BLACK);
   }
 
+  //グラフ線の描画
+  int leftMargin = 0;
+  for (int i = 0; i<len+1; i++){
+    float prev;
+    float current;
+    if(i==0){
+       prev = 0;
+    }if(i==len+1){
+      current = 0;
+    }else{
+       prev=sensVals[i-1];
+       current = sensVals[i];
+    }
+    
+    M5.Lcd.drawLine(leftMargin+(i*35), 200-(prev/255)*200, leftMargin+((i+1)*35), 200-(current/255)*200, TFT_WHITE);
+  }
+  delay(100);
+
+  
+  //グラフ線の消去
+  for (int i = 0; i<len+1; i++){
+    float prev;
+    float current;
+    if(i==0){
+       prev = 0;
+    }if(i==len+1){
+      current = 0;
+    }else{
+       prev=sensVals[i-1];
+       current = sensVals[i];
+    }
+    M5.Lcd.drawLine(leftMargin+(i*35), 200-(prev/255)*200, leftMargin+((i+1)*35), 200-(current/255)*200, TFT_BLACK);
+  }
    
   Serial.println();
   M5.Lcd.println();
   
   //delay(100);
  }
-  
 
